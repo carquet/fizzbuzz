2946f540ba60fb2a33826f2f91afa36621937185
L.Carquet <laetiticarquet@gmail.com>
2016-07-20 14:40:41 +0200

    adds bang to numbers starting with 1

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,24 @@
 for i in 1..100
-	if i % 3 == 0 && i % 5 == 0
-		puts "fizzbuzz"
+	if i % 3 == 0 && i % 5 == 0 
+		if i.to_s.start_with?"1"
+			puts "fizzbuzzbang"
+		else
+		puts "fizzbuzz"	
+		end		
 	elsif i % 3 == 0
-		puts "fizz"
+		if i.to_s.start_with?"1"
+		puts "fizzbang"
+		else
+			puts "fizz"
+		end
 	elsif i % 5 == 0
+		if i.to_s.start_with?"1"
+		puts "buzzbang"
+		else
 		puts "buzz"
+		end
+	elsif i.to_s.start_with?"1"
+		puts "bang"
 	else 
 		puts i
 	end
