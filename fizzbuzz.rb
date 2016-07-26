ef7e1025d1de58e855303f267c76d31a37149739
L.Carquet <laetiticarquet@gmail.com>
2016-07-26 15:09:08 +0200

    refactore both loop, iteratin4 complete

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,25 +1,25 @@
+
+
 for i in 1..100
-	if i % 3 == 0 && i % 5 == 0 
-		if i.to_s.start_with?"1"
-			puts "fizzbuzzbang"
-		else
-		puts "fizzbuzz"	
-		end		
-	elsif i % 3 == 0
-		if i.to_s.start_with?"1"
-		puts "fizzbang"
-		else
-			puts "fizz"
-		end
-	elsif i % 5 == 0
-		if i.to_s.start_with?"1"
-		puts "buzzbang"
-		else
-		puts "buzz"
-		end
-	elsif i.to_s.start_with?"1"
-		puts "bang"
-	else 
-		puts i
+	
+	result = ""
+
+	if i % 3 == 0 
+		result +=  "Fizz"
 	end
+
+	if i % 5 == 0
+		result += "Buzz"
+	end
+
+	if i.to_s.start_with?"1"
+		result += "Bange"
+	end
+
+	if result == ""
+		result = i
+	end
+
+print result
 end
+
--- fizzbuzz2.rb
+++ fizzbuzz2.rb
@@ -1,13 +1,21 @@
 i = 0
 while i <= 99
 	i += 1
-	if i % 3 == 0 && i % 5 == 0
-		puts "FizzBuzz"
-	elsif i % 3 == 0 
-		puts "Fizz"
-	elsif i % 5 == 0
-		puts "Buzz"
-	else
-		puts i
+	result = ""
+
+
+	if i % 3 == 0 
+		result +=  "Fizz"
 	end
+	if i % 5 == 0 
+		result += "Buzz"
+	end
+	if i.to_s.start_with?"1"
+		result += "bang"
+	end
+	if result == ""
+		result = i
+	end
+
+print result
 end
